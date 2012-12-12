9a40b3e3cd1c26d4a225ed006f77a60c4c17dfa8
Micah <micahmartin@gmail.com>
2012-12-12 12:28:38 -0600

    adds more questions and specs

--- spec/xmas/questions_spec.clj
+++ spec/xmas/questions_spec.clj
@@ -1,11 +1,36 @@
 (ns xmas.questions-spec
-  (:require [speclj.core :refer :all]
-            [xmas.questions :refer :all]))
+  (:require [speclj.core :refer :all ]
+            [clojure.java.io :refer [resource]]
+            [xmas.questions :refer :all ]))
 
 (describe "Questions"
 
-    (it "has 50 questions"
-      (should= 50 (count questions)))
+  (it "has 50 questions"
+    (should= 50 (count questions)))
 
-    )
+  (it "each question has an icon"
+    (doseq [q questions]
+      (should-contain :icon q)
+      (should= clojure.lang.Keyword (class (:icon q)))))
+
+  (it "each icon has an image"
+    (doseq [q questions]
+      (let [filename (format "images/%s.png" (name (:icon q)))]
+        (should-contain (name (:icon q)) (str (resource filename))))))
+
+  (it "each clue has a question and answer"
+    (doseq [c questions]
+      (should-contain :q c)
+      (should-contain :a c)))
+
+  (it "each icon is unique"
+    (should= 50 (count (into #{} (map :icon questions)))))
+
+  (it "each question is unique"
+    (should= 50 (count (into #{} (map :q questions)))))
+
+  (it "each answer is unique"
+    (should= 50 (count (into #{} (map :a questions)))))
+
+  )
 
--- src/xmas/questions.clj
+++ src/xmas/questions.clj
@@ -2,41 +2,56 @@
 
 (def questions
   [
-   {:a "dunno" :q "What is Myles' middle name?"}
-   {:a "cards" :q "8th Light's very first project involved a pink box.  What was kept in that box?"}
-   {:a "ruby & applescript" :q "What programming language controls the wall-mounted TV's web page cycle?"}
-   {:a "zero" :q "How many Rails apps had Micah and Paul written when 8th Light signed their first paying Rails gig?"}
-   {:a "PCReservation" :q "What 8th Light developed software runs in the Chicago Public Library?"}
-   {:a "Angelique Martin" :q "I was born on a Carribean Island and can understand Creole. Who am I?"}
-   {:a "Craig Demyanovich" :q "This craftsman dreams of sticks, disks, and lots-o mighty cold H2O"}
-   {:a "Eric Meyer" :q "I have a master on each shoulder.  One light.  One dark.  Who am I?"}
-   {:a "Josh Cheek" :q "If you needed a knitted snifter warmer, which craftsman should you seek?"}
-   {:a "Paul Pagel" :q "Which craftsman rides a Triumph?"}
-   {:a "Wai Lee" :q "Which craftsman most resembles Beatles rock star, George Harrison?"}
-   {:a "Skim" :q "Which craftsman owns a bright yellow drifer car in another state?"}
-   {:a "Sue Kim" :q "Which craftsman lives with a retired proffessional track runner?"}
-   {:a "Jim Suchy" :q "8th Light Employee #1"}
-   {:a "Doug Bradbury" :q "This craftman dug through other people's crap to produce a gem that thousands of craftsman can attest to."}
-   {:a "Colin Jones" :q "Undoubtedl-y, this craftman-y can ver-y cleverl-y name things-y."}
-   {:a "Margaret Pagel" :q "This craftsman was in the dental industry prior to joining 8th Light."}
-   {:a "Susan Rosso" :q ""}
-   {:a "Dave Moore" :q "I go to war almost every other weekend. No one dies and if I'm lucky, I get paid.  Who am I?"}
-   {:a "Michael Baker" :q ""}
-   {:a "Stephanie Briones" :q ""}
-   {:a "Patrick Gombert" :q ""}
-   {:a "Mark Grant" :q ""}
-   {:a "Ginny Hendry" :q ""}
-   {:a "Mike Jansen" :q ""}
-   {:a "Keven Liddle" :q ""}
-   {:a "Li-Hsuan Lung" :q ""}
-   {:a "Jeremy Neander" :q ""}
-   {:a "Dariuz Pasciak" :q ""}
-   {:a "Chris Peak" :q ""}
-   {:a "Brian Pratt" :q ""}
-   {:a "Cymen Vig" :q ""}
-   {:a "Ben Voss" :q ""}
-   {:a "Eric Koslow" :q "Had this craftsman from the big apple chosen the wrong school, he'd be speaking Japaneese instead of writting code."}
-   {:a "Nick Meccia" :q "Which craftsman has produced their very own music comic book?"}
+   {:icon :balloons :a "dunno" :q "What is Myles' middle name?"}
+   {:icon :blue-bow :a "cards" :q "8th Light's very first project involved a pink box.  What was kept in that box?"}
+   {:icon :blue-flake :a "ruby & applescript" :q "What programming language controls the wall-mounted TV's web page cycle?"}
+   {:icon :blue-gift :a "zero" :q "How many Rails apps had Micah and Paul written when 8th Light signed their first paying Rails gig?"}
+   {:icon :blue-ornament :a "PCReservation" :q "What 8th Light developed software runs in the Chicago Public Library?"}
+   {:icon :blue-star :a "Angelique Martin" :q "I was born on a Carribean Island and can understand Creole. Who am I?"}
+   {:icon :cake :a "Craig Demyanovich" :q "This craftsman dreams of sticks, disks, and lots-o mighty cold H2O"}
+   {:icon :candy-cane :a "Eric Meyer" :q "I have a master on each shoulder.  One light.  One dark.  Who am I?"}
+   {:icon :candy-house :a "Josh Cheek" :q "If you needed a knitted snifter warmer, which craftsman should you seek?"}
+   {:icon :christmas-tree :a "Paul Pagel" :q "Which craftsman rides a Triumph?"}
+   {:icon :christmas-card :a "Wai Lee" :q "Which craftsman most resembles Beatles rock star, George Harrison?"}
+   {:icon :cookies :a "Skim" :q "Which craftsman owns a bright yellow drifer car in another state?"}
+   {:icon :gift-tags :a "Sue Kim" :q "Which craftsman lives with a retired proffessional track runner?"}
+   {:icon :ginger-guy :a "Jim Suchy" :q "8th Light Employee #1"}
+   {:icon :ginger-stocking :a "Doug Bradbury" :q "This craftman dug through other people's crap to produce a gem that thousands of craftsman can attest to."}
+   {:icon :ginger-tree :a "Colin Jones" :q "Undoubtedl-y, this craftman-y can ver-y cleverl-y name things-y."}
+   {:icon :gold-bell :a "Margaret Pagel" :q "This craftsman was in the dental industry prior to joining 8th Light."}
+   {:icon :red-candles :a "Eric Koslow" :q "Had this craftsman from the big apple chosen the wrong school, he'd be speaking Japaneese instead of writting code."}
+   {:icon :red-gift :a "Nick Meccia" :q "Which craftsman has produced their very own music comic book?"}
+   {:icon :gold-gift :a "Dave Moore" :q "I go to war almost every other weekend. No one dies and if I'm lucky, I get paid.  Who am I?"}
+   {:icon :green-gift :a "Patrick Gombert" :q "This craftsman has double jointed elbows (wierd!) and was voted 'worst case of senioritus' in high school."}
+   {:icon :icy-earth :a "Keven Liddle" :q "This craftman has never experienced a brain freeze and has been known to sprinkle the day with dance moves."}
+   {:icon :gold-flake :a "Susan Rosso" :q "This saxaphone playing craftsman is a fan of Leonard Nimoy as Spok from the classic Star Trek."}
+   {:icon :pink-ornament :a "Brian Pratt" :q "This chili cooking champ of a craftsman is scared of hair cuts."}
+   {:icon :polkadot-gift :a "Cymen Vig" :q "This craftsman is, perhaps was, a cow milker and rusty pick-up truck owner."}
+   {:icon :holly :a "Mike Jansen" :q "You'd be calling this craftsman Spike or Butch if their old-man had their way."}
+   {:icon :gold-star :a "Michael Baker" :q "This craftsman was the proud owner of a sugar glider (WTF?) and nearly became a humanities teacher."}
+   {:icon :igloo :a "Li-Hsuan Lung" :q "This craftmsn has family ties with the mafia and an army general.  That translates to power!"}
+   {:icon :heart :a "Ginny Hendry" :q "This self-qualifiying 'tall' craftsman hates the color pink."}
+   {:icon :green-bow :a "Stephanie Briones" :q "This nomadic craftsman has lived in 5 states and would be playing the drums today if not for the close proximity of Chicago neighbors."}
+   {:icon :jingle-bell :a "Jeremy Neander" :q "This craftsman has a soft spot for meandering blank-and-white animals."}
+   {:icon :mistletoe :a "Dariuz Pasciak" :q "This craftsman eats chicken soup 6 times a week.  How Daring!"}
+   {:icon :green-ornament :a "Mark Grant" :q ""}
+   {:icon :pink-flake :a "Chris Peak" :q ""}
+   {:icon :red-bow :a "Ben Voss" :q ""}
+   {:icon :red-ornament}
+   {:icon :red-star}
+   {:icon :reindeer}
+   {:icon :santa-hat}
+   {:icon :santa-letter}
+   {:icon :santa-sack}
+   {:icon :sleigh}
+   {:icon :snow-globe}
+   {:icon :snowballz}
+   {:icon :snowman}
+   {:icon :snowy-house}
+   {:icon :stocking}
+   {:icon :teddy-gift}
+   {:icon :treasure-chest}
+   {:icon :wreath}
   ])
 
 
