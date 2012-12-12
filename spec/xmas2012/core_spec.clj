5f97ef798494dd2be44913b071903b75821783e9
Micah <micahmartin@gmail.com>
2012-12-11 20:39:15 -0600

    adding files

---
+++ spec/xmas2012/core_spec.clj
@@ -0,0 +1,7 @@
+(ns xmas2012.core-spec
+  (:require [speclj.core :refer :all]
+            [xmas2012.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/xmas2012/core.clj
@@ -0,0 +1,1 @@
+(ns xmas2012.core)
